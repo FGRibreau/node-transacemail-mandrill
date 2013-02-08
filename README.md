9884e905bc21d0347c969877c914a40ceca2f0ed
FG Ribreau <github@fgribreau.com>
2013-02-08 10:58:23 +0100

    Support options + debugMode

--- README.md
+++ README.md
@@ -5,9 +5,26 @@ Usage
 =====
 
 ```JavaScript
-var Mailing = require('node-transacemail');
+var Mailing = require('transacemail');
 
 var mails = Mailing
-              .setMailProvider(require('transacemail-mandrill')('apikey'))
-              .compile('/path/to/mails');
+              .compile('/path/to/mails')
+              .setMailProvider(require('transacemail-mandrill')('apikey'));
+
+// OR
+var Mandrill = require('transacemail-mandrill');
+
+var mails = Mailing
+              .compile('/path/to/mails')
+              .setMailProvider(Mandrill('apikey', {
+                track_opens: true,
+                track_clicks: true,
+                auto_text:false,
+                subject: "node-transacemail-mandrill",
+                from_email: "mandrill@transacemail.com",
+                from_name: "Francois-Guillaume Ribreau"
+              }));
+
+
+mails.setMailProvider();
 ```
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-var Mandrill = require('node-mandrill');
-var _        = require('lodash');
+var Mandrill   = require('node-mandrill');
+var _          = require('lodash');
 var deepExtend = require('deep-extend');
 
 function MailProvider(apiKey, options){
+  if(!apiKey){
+    throw new Error("require('transacemail-mandrill')('apikey'), api key is required.");
+  }
+
   this.options  = deepExtend({}, MailProvider.DEFAULT_OPTION, options);
   this.mandrill = Mandrill(apiKey);
 }
 
+/**
+ * Debug mode
+ * @type {Boolean}
+ */
+MailProvider.DEBUG_MODE = false;
+
+/**
+ * Default options
+ * @type {Object}
+ */
 MailProvider.DEFAULT_OPTION = {
-  messsage:{
+  message:{
     track_opens: true,
     track_clicks: true,
     auto_text:false,
@@ -18,20 +32,35 @@ MailProvider.DEFAULT_OPTION = {
   }
 };
 
+/**
+ * Send method
+ * @param  {Object}   mail TransacEmail Mail object
+ * @param  {Function} fn   Callback to call when done
+ */
 MailProvider.prototype.send = function(mail, fn){
   if(!mail.data || !mail.data.email){
     throw new Error("The `email` field is required");
   }
 
   var options = deepExtend({}, this.options, mail.mandrill, {
-    html:mail.html,
-    text:mail.text,
-    /**
-     * If mail.text is not defined, autotext will be set to true
-     * @type {Boolean}
-     */
-    auto_text:!mail.text,
     message:{
+      /**
+       * If mail.text is not defined, autotext will be set to true
+       * @type {Boolean}
+       */
+      auto_text:!mail.text,
+
+      /**
+       * HTML content
+       * @type {String}
+       */
+      html:mail.html,
+
+      /**
+       * Text content
+       * @type {String}
+       */
+      text:mail.text,
       to:[{
           email:mail.data.email,
           name: mail.data.name
@@ -39,7 +68,14 @@ MailProvider.prototype.send = function(mail, fn){
     }
   });
 
-  mandrill('/messages/send', options, function(error, response){
+  if(MailProvider.DEBUG_MODE){
+    console.log("\nSending...");
+    console.log(JSON.stringify(options, null, 2));
+    fn();
+    return;
+  }
+
+  this.mandrill('/messages/send', options, function(error, response){
     //everything's good, lets see what mandrill said
     //[ { email: 'plop@fgribreau.com', status: 'sent' } ]
     fn(error ? JSON.stringify(error): null, response);
@@ -47,16 +83,13 @@ MailProvider.prototype.send = function(mail, fn){
 };
 
 /**
- * [exports description]
+ * TransacEmail-Mandrill factory
  * @param  {String} apiKey        Mandrill API Key
  * @param  {Object} defaultOption Default send option
  * @return {MailProvider}
  */
 function Factory(apiKey, defaultOption){
-  if(!apiKey){
-    throw new Error("require('transacemail-mandrill')('apikey'), api key is required.");
-  }
-  return new MailProvider(apiKey, defaultOption || {});
+  return new MailProvider(apiKey, defaultOption || {});
 }
 
 Factory.MailProvider = MailProvider;
